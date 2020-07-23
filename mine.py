3f18fbe9274d4062b1aaa0350c019d94ab01386f
Jack Temple <jetemple@iastate.edu>
2020-07-23 14:18:35 -0700

    Cleaned up mine.py

--- mine.py
+++ mine.py
@@ -12,26 +12,8 @@ from string import punctuation
 sys.path.insert(0, 'vaderSentiment/vaderSentiment')
 from vaderSentiment import SentimentIntensityAnalyzer
 
-
-# contains client_id and client_secret used to connect
-
-file1 = open("myfile.txt", "w")
-
-# sys.stdout = file1
-# TIME_PERIOD = 60 * 60 * 24 * 1
-TIME_PERIOD = 60 * 60 * 6
-
+TIME_PERIOD = 60 * 60 # How far you want to go back in the subreddit
 SUBREDDIT = 'wallstreetbets'
-STOCK_SPECIFIC_METION_WEIGHT = 5
-STOCK_POST_MENTION_WEIGHT = 3
-STOCK_DEFAULT_WEIGHT = 1
-
-# handler = logging.StreamHandler()
-# handler.setLevel(logging.DEBUG)
-# logger = logging.getLogger('prawcore')
-# logger.setLevel(logging.DEBUG)
-# logger.addHandler(handler)
-
 
 class Ticker:
     def __init__(self, ticker):
@@ -65,13 +47,8 @@ class Ticker:
 # list of common english words to remove
 common_word_filters = ["AUG", "CEO", "GOLD", "ALOT", "JAN", "ONCE", "EDIT", "BRO", "SU", "LIFE", "CFO", "JOB", "BIT", "TWO", "BEST", "BIG", "EOD", "HOPE", "AM", "EVER", "PUMP", "NEXT", "HE", "REAL", "WORK", "NICE", "TOO", "MAN", "LOVE", "BY", "VERY", "ANY", "SEE",
                        "NEW", "WELL", "TELL", "IT", "ONE", "POST", "ON", "TURN", "GOOD", "CAN", "HAS", "GO", "PLAY", "ELSE", "GAIN", "RUN", "INFO", "STAY", "CARE", "ALL", "AT", "PER", "DO", "ARE", "NOW", "BE", "OR", "SO", "OUT", "BEAT", "AGO", "AN", "PEAK", "LOW", "DD", "FOR", "FLAT"]
-common_word_translations = {"AVIS": "CAR", "SPOTIFY": "SPOT", "TESLA": "TSLA", "Lockheed": "LMU", "MICRON": "MU", "SHOPIFY": "SHOP", "DISNEY": "DIS",
-                            "Boeing": "BA", "NETFLIX": "NFLX", "APPLE": "AAPL", "INTEL": "INTC", "PELOTON": "PTON", "PELETON": "PTON", "GOOGLE": "GOOG", "STARBUCKS": "SBUX"}
-word_counts = {}
-stocks = {}
-specific_stocks = {}
-ticker_dict = {}
 
+ticker_dict = {} # Holds all of the tickers
 
 # Checks to see if there are tickers in the word
 def analyze_text(text):
@@ -81,21 +58,19 @@ def analyze_text(text):
         if (len(word) < 3):
             continue
 
-        #Does word fit the ticker criteria
+        # Does word fit the ticker criteria
         if word.isupper() and len(word) != 1 and (word.upper() not in common_word_filters) and len(word) <= 5 and word.isalpha():
             with open("cache.json") as file, open("false-positives.json") as file2:
                 data = json.load(file)
                 data2 = json.load(file2)
-                #Checks to see if the ticker has been cached.
+                # Checks to see if the ticker has been cached.
                 if (word not in data) and (word not in data2):
-                    #HERE WHY DOES NKLA NOT WORK
-                    try: #Add ticker to cache 
+                    try: # Add ticker to cache 
                         company = yf.Ticker(word).info["longName"]
                         add = {word : company}
                         data.update(add)
                         with open("cache.json", "w") as f:
                             json.dump(data,f)
-                        print(company) #TESTING
                     except Exception as e:
                         print(e, word)
                         if(word not in data2):
@@ -103,7 +78,6 @@ def analyze_text(text):
                             data2.update(add)
                             with open("false-positives.json", "w") as f:
                                 json.dump(data2,f)
-                        # print("THIS AINT A WORD DUMBASS") #TESTING
                         continue
             if (word in data) and (word in ticker_dict):
                 ticker_dict[word].count += 1
@@ -126,24 +100,11 @@ def crawl_subreddit(subreddit):
         time_delta = timestamp - submission.created_utc
         if (time_delta > TIME_PERIOD):
             break
-        # print("Title:\t", submission.title)
-        # print("ID:\t", submission.id)
-        # print("Comments:\t", submission.num_comments)
-        # print("Age:\t", time_delta)
-        # Parses post title and content
         ticker_dict = analyze_text(submission.title)
         ticker_dict = analyze_text(submission.selftext)
-
-        #submission.comments.replace_more(limit=None, threshold=0)
-        # if submission.num_comments > 500:
-        #     continue
-        # print("Getting more comments (", submission.num_comments, "), this may take a while...")
-
         # Parses post comments
         submission.comments.replace_more(limit=None, threshold=0)
         for comment in submission.comments.list():
-            #
-            #  print("\t", comment.id)
             ticker_dict = analyze_text(comment.body)
 
 
@@ -155,36 +116,3 @@ for ticker in ticker_dict:
 
 for key, value in sorted(count.items(), key=lambda x: x[1], reverse=True):
     print(key, value)
-# for ticker in ticker_dict:
-#     cur = ticker_dict[ticker]
-#     # print(ticker_dict[ticker].bullish)
-#     # print(ticker_dict[ticker].bearish)
-#     # print(ticker_dict[ticker].neutral)
-#     Ticker.analyze_sentiment(cur)
-#     if cur.count > 0:
-#         print("STOCK", cur.ticker)
-#         print("Bullish", cur.bullish)
-#         print("Bearish", cur.bearish)
-#         print("Neutral", cur.neutral)
-    # print(ticker[0])
-#     if(stock == "TSLA"):
-#         print("FUCK")
-#         print(stocks[stock].count)
-#     if((stocks[stock].count) > 0):
-#         Ticker.analyze_sentiment(stocks[stock])
-#         print("=====================================")
-#         print(stock)
-#         print(stocks[stock].bodies)
-#         print("Bearish:", stocks[stock].bearish, "%")
-#         print("Bullish:", stocks[stock].bullish, "%")
-#         print("Neutral:", stocks[stock].neutral, "%")
-#     # print(stocks[stock].count)
-#     # if stock.count is not 0:
-#     #     final_stocks[stock] = stock
-#     #     print(final_stocks[stock].count)
-#     # print(stock)
-
-# stocks = {k: v for k, v in sorted(specific_stocks.items(), key=lambda item: item[1], reverse=True)}
-# print(stocks)
-
-file1.close()
