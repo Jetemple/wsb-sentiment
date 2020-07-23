3860ee31c958255ef8a21bf03ff5e945735a9693
Jack Temple <jetemple@iastate.edu>
2020-07-22 17:23:45 -0700

    Making progress

---
+++ config.py
@@ -0,0 +1,3 @@
+client_id='3qq7C5ay-Ch0Og'
+client_secret='Fce6UBFEDvpkaXnfF0kkwnowdGk'
+alpha_vantage_api='0Q33PX2P7UIJO0BV'
--- mine.py
+++ mine.py
@@ -1,169 +1,180 @@
+import alphavantage
+import config
+from vaderSentiment import SentimentIntensityAnalyzer
 import csv
 import praw
 import time
 import os
 import logging
 import sys
+import yfinance as yf
 from string import punctuation
 sys.path.insert(0, 'vaderSentiment/vaderSentiment')
-from vaderSentiment import SentimentIntensityAnalyzer
 
 
-#contains client_id and client_secret used to connect
-import config
+# contains client_id and client_secret used to connect
 
-file1 = open("myfile.txt","w")
+file1 = open("myfile.txt", "w")
 
-sys.stdout = file1
+# sys.stdout = file1
 
-# TIME_PERIOD						= 60 * 60 * 4 * 1
-TIME_PERIOD						= 60 * 20
+TIME_PERIOD = 60 * 60 * 24 * 1
+# TIME_PERIOD = 60 * 20
 
-SUBREDDIT						= 'wallstreetbets'
-STOCK_SPECIFIC_METION_WEIGHT 	= 5
-STOCK_POST_MENTION_WEIGHT 		= 3
-STOCK_DEFAULT_WEIGHT			= 1
+SUBREDDIT = 'wallstreetbets'
+STOCK_SPECIFIC_METION_WEIGHT = 5
+STOCK_POST_MENTION_WEIGHT = 3
+STOCK_DEFAULT_WEIGHT = 1
 
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-logger = logging.getLogger('prawcore')
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+# handler = logging.StreamHandler()
+# handler.setLevel(logging.DEBUG)
+# logger = logging.getLogger('prawcore')
+# logger.setLevel(logging.DEBUG)
+# logger.addHandler(handler)
+
 
 class Ticker:
-   def __init__(self, ticker):
-      self.ticker = ticker
-      self.count = 0
-      self.bodies = []
-      self.pos_count = 0
-      self.neg_count = 0
-      self.bullish = 0
-      self.bearish = 0
-      self.neutral = 0
-      self.sentiment = 0 # 0 is neutral
+    def __init__(self, ticker):
+        self.ticker = ticker
+        self.count = 0
+        self.bodies = []
+        self.pos_count = 0
+        self.neg_count = 0
+        self.bullish = 0
+        self.bearish = 0
+        self.neutral = 0
+        self.sentiment = 0  # 0 is neutral
 
-   def analyze_sentiment(self):
-      analyzer = SentimentIntensityAnalyzer()
-    #   print("HERE")
-    #   print(self)
-      neutral_count = 0
-      for text in self.bodies:
-         sentiment = analyzer.polarity_scores(text)
-         if (sentiment["compound"] > .005) or (sentiment["pos"] > abs(sentiment["neg"])):
-            self.pos_count += 1
-         elif (sentiment["compound"] < -.005) or (abs(sentiment["neg"]) > sentiment["pos"]):
-            self.neg_count += 1
-         else:
-            neutral_count += 1
+    def analyze_sentiment(self):
+        analyzer = SentimentIntensityAnalyzer()
+        neutral_count = 0
+        for text in self.bodies:
+            sentiment = analyzer.polarity_scores(text)
+            if (sentiment["compound"] > .005) or (sentiment["pos"] > abs(sentiment["neg"])):
+                self.pos_count += 1
+            elif (sentiment["compound"] < -.005) or (abs(sentiment["neg"]) > sentiment["pos"]):
+                self.neg_count += 1
+            else:
+                neutral_count += 1
 
-      self.bullish = int(self.pos_count / len(self.bodies) * 100)
-      self.bearish = int(self.neg_count / len(self.bodies) * 100)
-      self.neutral = int(neutral_count / len(self.bodies) * 100)
+        self.bullish = int(self.pos_count / len(self.bodies) * 100)
+        self.bearish = int(self.neg_count / len(self.bodies) * 100)
+        self.neutral = int(neutral_count / len(self.bodies) * 100)
 
 
+# list of common english words to remove
+common_word_filters = ["AUG", "CEO", "GOLD", "ALOT", "JAN", "ONCE", "EDIT", "BRO", "SU", "LIFE", "CFO", "JOB", "BIT", "TWO", "BEST", "BIG", "EOD", "HOPE", "AM", "EVER", "PUMP", "NEXT", "HE", "REAL", "WORK", "NICE", "TOO", "MAN", "LOVE", "BY", "VERY", "ANY", "SEE",
+                       "NEW", "WELL", "TELL", "IT", "ONE", "POST", "ON", "TURN", "GOOD", "CAN", "HAS", "GO", "PLAY", "ELSE", "GAIN", "RUN", "INFO", "STAY", "CARE", "ALL", "AT", "PER", "DO", "ARE", "NOW", "BE", "OR", "SO", "OUT", "BEAT", "AGO", "AN", "PEAK", "LOW", "DD", "FOR", "FLAT"]
+common_word_translations = {"AVIS": "CAR", "SPOTIFY": "SPOT", "TESLA": "TSLA", "Lockheed": "LMU", "MICRON": "MU", "SHOPIFY": "SHOP", "DISNEY": "DIS",
+                            "Boeing": "BA", "NETFLIX": "NFLX", "APPLE": "AAPL", "INTEL": "INTC", "PELOTON": "PTON", "PELETON": "PTON", "GOOGLE": "GOOG", "STARBUCKS": "SBUX"}
+word_counts = {}
+stocks = {}
+specific_stocks = {}
+ticker_dict = {}
 
-#list of common english words to remove
-common_word_filters=["GOLD", "ALOT", "JAN", "ONCE", "EDIT", "BRO", "SU", "LIFE", "CFO", "JOB", "BIT", "TWO", "BEST", "BIG", "EOD", "HOPE", "AM", "EVER", "PUMP", "NEXT", "HE", "REAL", "WORK", "NICE", "TOO", "MAN", "LOVE", "BY", "VERY", "ANY", "SEE", "NEW", "WELL", "TELL", "IT", "ONE", "POST", "ON", "TURN", "GOOD", "CAN", "HAS", "GO", "PLAY", "ELSE", "GAIN", "RUN", "INFO", "STAY", "CARE", "ALL", "AT", "PER", "DO", "ARE", "NOW", "BE", "OR", "SO", "OUT", "BEAT", "AGO", "AN", "PEAK", "LOW", "DD", "FOR", "FLAT"]
-common_word_translations={"AVIS" : "CAR", "SPOTIFY" : "SPOT", "TESLA" : "TSLA", "Lockheed" : "LMU", "MICRON" : "MU", "SHOPIFY" : "SHOP", "DISNEY" : "DIS", "Boeing" : "BA", "NETFLIX" : "NFLX", "APPLE" : "AAPL", "INTEL" : "INTC", "PELOTON" : "PTON", "PELETON" : "PTON", "GOOGLE" : "GOOG", "STARBUCKS" : "SBUX"}
-word_counts={}
-stocks={}
-specific_stocks={}
 
-#Load base list of stock symbols from all csv files in cwd
-#the first column must be the stock symbol
-for filename in os.listdir(os.getcwd()):
-    if filename.endswith(".csv"): 
-        # print("Loading {}...".format(filename))
-        
-        with open(filename, mode='r') as infile:
-            reader = csv.reader(infile)
-            for rows in reader:
-                Ticker(rows[0])
-                stocks[Ticker(rows[0]).ticker] = Ticker(rows[0])
-print("HERE")
-print(stocks)
-
-#analyze a blob of text, increment counts in stock dictionary
-#add new entries for any words thats start with a $
-def analyze_text(text, value=STOCK_DEFAULT_WEIGHT):
+# Checks to see if there are tickers in the word
+def analyze_text(text):
     for word in text.split():
-        word=word.upper()
         word = word.rstrip(punctuation)
 
-        
-        if (len(word)<3):
+        if (len(word) < 3):
             continue
-            
-        if ( (word.isalnum()) or ( (word[0] is "$") and (word[1:].isalpha()) )):
-            if word in stocks and (word not in common_word_filters):
-                
-                # print(stocks[word].ticker)
-                stocks[word].bodies.append(text)
-                stocks[word].count = stocks[word].count + 1
-                # print("FUCK YEAHHHH")
-                # print(stocks[word].count)
-                # print(stocks[word].bodies)
+        if word.isupper() and len(word) != 1 and (word.upper() not in common_word_filters) and len(word) <= 5 and word.isalpha():
+            try:
+                yf.Ticker(word).cashflow
+            except:
+                # print("THIS AINT A WORD DUMBASS")
+                continue
 
-            if word in word_counts:
-                word_counts[word] += value
+        # Used to speed up the process, so I don't have to
+            # print(stocks[word].ticker)
+            # stocks[word].bodies.append(text)
+            # stocks[word].count = stocks[word].count + 1
+            # print("FUCK YEAHHHH")
+            # print(stocks[word].count)
+            # print(stocks[word].bodies)
+
+            if word in ticker_dict:
+                ticker_dict[word].count += 1
+                ticker_dict[word].bodies.append(text)
             else:
-                word_counts[word] = value
+                ticker_dict[word] = Ticker(word)
+                ticker_dict[word].count = 1
+                ticker_dict[word].bodies.append(text)
+    return ticker_dict
 
 
-# print("Crawling subreddit[{}] for all posts/comments within the last {} seconds".format(SUBREDDIT, TIME_PERIOD))
+def crawl_subreddit(subreddit):
+    # Create praw connection
+    reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret,
+                         user_agent='Comment extraction by /u/PartialSyntax')
 
-#Create praw connection	
-reddit = praw.Reddit(client_id=config.client_id, client_secret=config.client_secret, user_agent='Comment extraction by /u/rizir')
+    # iterate through latest 24 hours of submissions and all comments made on those submissions
+    timestamp = int(time.time())
+    for submission in reddit.subreddit(SUBREDDIT).new(limit=1000):
+        time_delta = timestamp - submission.created_utc
+        if (time_delta > TIME_PERIOD):
+            break
+        # print("Title:\t", submission.title)
+        # print("ID:\t", submission.id)
+        # print("Comments:\t", submission.num_comments)
+        # print("Age:\t", time_delta)
+        # Parses post title and content
+        ticker_dict = analyze_text(submission.title)
+        ticker_dict = analyze_text(submission.selftext)
 
-#iterate through latest 24 hours of submissions and all comments made on those submissions
+        #submission.comments.replace_more(limit=None, threshold=0)
+        # if submission.num_comments > 500:
+        #     continue
+        # print("Getting more comments (", submission.num_comments, "), this may take a while...")
 
-timestamp	= int(time.time())
+        # Parses post comments
+        submission.comments.replace_more(limit=None, threshold=0)
+        for comment in submission.comments.list():
+            # print("\t", comment.id)
+            ticker_dict = analyze_text(comment.body)
 
-for submission in reddit.subreddit(SUBREDDIT).new(limit=1000):
-    time_delta=timestamp - submission.created_utc
 
-    if (time_delta > TIME_PERIOD):
-        break
+crawl_subreddit("wallstreetbets")
 
-    print("Title:\t", submission.title)
-    print("ID:\t", submission.id)
-    print("Comments:\t", submission.num_comments)
-    print("Age:\t", time_delta)
+count = {}
+for ticker in ticker_dict:
+    count[ticker] = ticker_dict[ticker].count
 
-    analyze_text(submission.title, value=STOCK_POST_MENTION_WEIGHT)
-    analyze_text(submission.selftext, value=STOCK_POST_MENTION_WEIGHT)
 
-    #submission.comments.replace_more(limit=None, threshold=0)
-    if submission.num_comments > 500:
-        print("Getting more comments (", submission.num_comments, "), this may take a while...")
-    submission.comments.replace_more(limit=None, threshold=0)
-    
-    for comment in submission.comments.list():
-        print("\t", comment.id)
-        analyze_text(comment.body)
+for key, value in sorted(count.items(), key=lambda x: x[1], reverse=True):
+    print(key, value)
+# for ticker in ticker_dict:
+#     cur = ticker_dict[ticker]
+#     # print(ticker_dict[ticker].bullish)
+#     # print(ticker_dict[ticker].bearish)
+#     # print(ticker_dict[ticker].neutral)
+#     Ticker.analyze_sentiment(cur)
+#     if cur.count > 0:
+#         print("STOCK", cur.ticker)
+#         print("Bullish", cur.bullish)
+#         print("Bearish", cur.bearish)
+#         print("Neutral", cur.neutral)
+    # print(ticker[0])
+#     if(stock == "TSLA"):
+#         print("FUCK")
+#         print(stocks[stock].count)
+#     if((stocks[stock].count) > 0):
+#         Ticker.analyze_sentiment(stocks[stock])
+#         print("=====================================")
+#         print(stock)
+#         print(stocks[stock].bodies)
+#         print("Bearish:", stocks[stock].bearish, "%")
+#         print("Bullish:", stocks[stock].bullish, "%")
+#         print("Neutral:", stocks[stock].neutral, "%")
+#     # print(stocks[stock].count)
+#     # if stock.count is not 0:
+#     #     final_stocks[stock] = stock
+#     #     print(final_stocks[stock].count)
+#     # print(stock)
 
-    print()
-
-final_stocks={}
-
-for stock in stocks:
-    if(stock == "TSLA"):
-        print("FUCK")
-        print(stocks[stock].count)
-    if((stocks[stock].count) > 0):
-        Ticker.analyze_sentiment(stocks[stock])
-        print("=====================================")
-        print(stock)
-        print(stocks[stock].bodies)
-        print("Bearish:", stocks[stock].bearish, "%")
-        print("Bullish:", stocks[stock].bullish, "%")
-        print("Neutral:", stocks[stock].neutral, "%")
-    # print(stocks[stock].count)
-    # if stock.count is not 0:
-    #     final_stocks[stock] = stock
-    #     print(final_stocks[stock].count)
-    # print(stock)
-    
+# stocks = {k: v for k, v in sorted(specific_stocks.items(), key=lambda item: item[1], reverse=True)}
+# print(stocks)
 
 file1.close()
-
