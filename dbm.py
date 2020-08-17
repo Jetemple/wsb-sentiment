d63d62617bcecdc5d89368ad23e61fa51653dc70
jet <jetemple@iastate.edu>
2020-08-16 21:45:21 -0700

    BETA-2 Cleanup

--- dbm.py
+++ dbm.py
@@ -17,17 +17,13 @@ def sql_connection():
 
 def addComment(id, date, ticker, post, body, sentiment):
     try:
-        # print("New COMMENT!")
         post = post[3:]
         con = sql_connection()
         cursorObj = con.cursor()
         cursorObj.execute("INSERT INTO comments VALUES (?, ?, ?, ?, ?, ?)",(id, date, ticker, post, body, sentiment))
         con.commit()
     except Exception as e:
-        # print(e)
         pass
-    #     pass
-    #     # print("Skipped")
 
 def addPost(id, comment_count, date):
     try:
@@ -37,7 +33,6 @@ def addPost(id, comment_count, date):
         con.commit()
     except:
         pass
-        # print("Skipped")
 
 def addTicker(ticker):
     try:
@@ -47,7 +42,6 @@ def addTicker(ticker):
         con.commit()
     except:
         pass
-        # print("STOCK ALREADY IN THERE")
 
 
 def checkTicker(ticker):
@@ -88,18 +82,3 @@ def getCommentCount(id):
     con.commit()
     val = cursorObj.fetchone()[0]
     return val
-
-    
-
-# con = sql_connection()
-# cursorObj = con.cursor()
-# cursorObj.execute("SELECT COUNT(1) FROM stocks WHERE ticker = 'TSLA'")
-# print(cursorObj.fetchone()[0])
-# con.commit()
-
-# print(checkComment("g1riw6c"))
-# print(checkTicker("tsla"))
-# addTicker("tsla")
-# sql_table(con)
-
-# getCommentCount("ib0hti")
